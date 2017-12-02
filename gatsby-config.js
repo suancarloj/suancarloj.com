478aed02080c8286a8cadd5e2095316dbec972c1
Juan Carlos Sanchez Cardenas <juan@qover.me>
2017-12-02 13:13:12 +0100

    Fix path to typographyjs in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `${__dirname}/src/utils/typography.js`,
+        pathToConfigModule: `src/utils/typography.js`,
       },
     },
     {
